cb7778120a30b703c11bf203b807f06d1c48fa32
DanielKuhnDeAraujo <danielaprendendo00@gmail.com>
2025-11-27 08:08:12 -0300

    forms

--- js/script.js
+++ js/script.js
@@ -634,3 +634,22 @@ document.addEventListener('DOMContentLoaded', function () {
         initializeFilters();
     }
 });
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
